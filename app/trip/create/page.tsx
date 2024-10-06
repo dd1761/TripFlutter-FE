e8308934304453fc98a3cf411ddfb468661aa775
HarenKei <seungmin1266@gmail.com>
2024-10-06 22:51:06 +0900

    Feat : Add trip pages router and basic components

---
+++ app/trip/create/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+import styled from "styled-components";
+import DefaultLayout from "@/app/components/DefaultLayout";
+
+const CreateTripPage: React.FC = () => {
+  return <></>;
+};
+
+export default CreateTripPage;
