55d44a3e66256315af7724e91f80d4855518a584
HarenKei <seungmin1266@gmail.com>
2024-10-09 21:09:14 +0900

    Feat : Add SVG Icon Components file & Implement Bottom Nav Bar

--- app/components/DefaultLayout.tsx
+++ app/components/DefaultLayout.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { styled } from "styled-components";
 import { COLORS } from "@/public/styles/colors";
+import BottomNav from "./commons/BottomNav";
 
 interface DefaultLayoutProps {
   // 자식 Node
@@ -29,7 +30,8 @@ const DefaultLayout: React.FC<DefaultLayoutProps> = ({
   return (
     <LayoutWrapper>
       <ContentsContainer top={top} right={right} bottom={bottom} left={left}>
-        <>{children}</>
+        <ChildrenContainer>{children}</ChildrenContainer>
+        <BottomNav />
       </ContentsContainer>
     </LayoutWrapper>
   );
@@ -56,6 +58,9 @@ const LayoutWrapper = styled.div`
 `;
 
 const ContentsContainer = styled.div<ContentsContainerProps>`
+  position: relative;
+  z-index: 1;
+
   display: flex;
   flex-direction: column;
 
@@ -67,6 +72,12 @@ const ContentsContainer = styled.div<ContentsContainerProps>`
     ${(props) => props.bottom}px ${(props) => props.left}px;
 
   overflow-x: auto;
+`;
+
+const ChildrenContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  margin-bottom: 72px;
 `;
 
 export default DefaultLayout;
---
+++ app/components/commons/BottomNav.tsx
@@ -0,0 +1,131 @@
+"use client";
+import styled from "styled-components";
+import { COLORS } from "@/public/styles/colors";
+import { PaperPlane, Community, GoHome, Suitcase, UserIcon } from "./Icons";
+import { useRouter, usePathname } from "next/navigation";
+
+interface NavInfo {
+  name: string;
+  route: string;
+  img: React.ReactNode;
+}
+
+const BottomNav: React.FC = () => {
+  const router = useRouter();
+  const currentPath = usePathname();
+
+  return (
+    <NavContainer>
+      <ItemContainer
+        onClick={() => {
+          router.push("/trip/create");
+        }}
+      >
+        <PaperPlane
+          color={
+            currentPath === "/trip/create" ? COLORS.mainColor : COLORS.greyColor
+          }
+          width={28}
+          height={28}
+        />
+        <ItemTitle>여행 계획</ItemTitle>
+      </ItemContainer>
+
+      <ItemContainer
+        onClick={() => {
+          router.push("/community");
+        }}
+      >
+        <Community
+          color={
+            currentPath === "/community" ? COLORS.mainColor : COLORS.greyColor
+          }
+          width={28}
+          height={28}
+        />
+        <ItemTitle>커뮤니티</ItemTitle>
+      </ItemContainer>
+
+      <ItemContainer
+        onClick={() => {
+          router.push("/");
+        }}
+      >
+        <GoHome
+          color={currentPath === "/" ? COLORS.mainColor : COLORS.greyColor}
+          width={28}
+          height={28}
+        />
+        <ItemTitle>홈</ItemTitle>
+      </ItemContainer>
+
+      <ItemContainer
+        onClick={() => {
+          router.push("/mypage/trip");
+        }}
+      >
+        <Suitcase
+          color={
+            currentPath === "/mypage/trip" ? COLORS.mainColor : COLORS.greyColor
+          }
+          width={28}
+          height={28}
+        />
+        <ItemTitle>내 여행</ItemTitle>
+      </ItemContainer>
+
+      <ItemContainer
+        onClick={() => {
+          router.push("/mypage");
+        }}
+      >
+        <UserIcon
+          color={
+            currentPath === "/mypage" ? COLORS.mainColor : COLORS.greyColor
+          }
+          width={28}
+          height={28}
+        />
+        <ItemTitle>마이페이지</ItemTitle>
+      </ItemContainer>
+    </NavContainer>
+  );
+};
+
+const NavContainer = styled.div`
+  position: fixed;
+  left: 50%;
+  bottom: 0;
+  z-index: 6;
+
+  transform: translate(-50%, 0);
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  min-width: 264px;
+  max-width: 544px;
+  width: calc(100% - 56px);
+  height: 58px;
+  padding: 8px 28px;
+
+  background-color: ${COLORS.whiteColor};
+  box-shadow: 0 -2px 10px rgba(0, 0, 0, 0.3);
+`;
+
+const ItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+`;
+
+const ItemTitle = styled.div`
+  font-size: 12px;
+  line-height: 12px;
+  font-weight: 700;
+  color: ${COLORS.greyColor};
+`;
+
+export default BottomNav;
