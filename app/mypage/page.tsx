b18b8fe6116cf3e913ef930982e710d7f456b395
HarenKei <seungmin1266@gmail.com>
2024-10-23 23:58:24 +0900

    Feat : Install React Redux

---
+++ app/mypage/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import styled from "styled-components";
+import DefaultLayout from "../components/DefaultLayout";
+import { COLORS } from "@/public/styles/colors";
+
+const MyPageIntroPage: React.FC = () => {
+  return (
+    <DefaultLayout top={0} right={0} bottom={0} left={0} nav={true}>
+      <></>
+    </DefaultLayout>
+  );
+};
+
+export default MyPageIntroPage;
