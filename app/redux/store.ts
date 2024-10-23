b88115cad0188c9ecca6eb71dbf67ef7333870d1
HarenKei <seungmin1266@gmail.com>
2024-10-24 00:18:59 +0900

    Feat : Add branch for not-login state

--- app/redux/store.ts
+++ app/redux/store.ts
@@ -6,13 +6,13 @@ const reducers = combineReducers({
   user: userSlice.reducer,
 })
 
-const middleware: (getDefaultMiddleware: any) => any = (getDefaultMiddleware) => {
+const middleware: (getDefaultMiddleware: any) => any = (getDefaultMiddleware) =>
+  // 개발 환경에서만 Redux-Logger를 추가합니다.
   process.env.NODE_ENV !== 'production' ?
     getDefaultMiddleware({ serializableCheck: false }).concat(logger) :
-    getDefaultMiddleware({ serialzableCheck: false });
+    // 배포 환경에서는 Redux-Logger를 추가하지 않습니다.
+    getDefaultMiddleware({ serializableCheck: false });
 
-
-};
 
 const store = configureStore({
   reducer: reducers,
