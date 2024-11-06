631900c21fa672dd35ec19d7c479204bbc9be069
KIMDONGBEEN <dd1761@naver.com>
2024-11-06 18:03:44 +0900

    관광지 리스트, 관광지 상세 페이지

--- pages/api/tour/tripDetail.js
+++ pages/api/tour/tripDetail.js
@@ -5,9 +5,10 @@ export default async function handler(req, res) {
         return res.status(405).json({ message: 'Method not allowed' });
     }
 
-    const { region } = req.query; // GET 요청이므로 쿼리로 region을 받습니다.
-    if (!region) {
-        return res.status(400).json({ message: 'Region parameter is required' });
+    const { region, title } = req.query;
+
+    if (!region && !title) {
+        return res.status(400).json({ message: 'Either Region or Title parameter is required' });
     }
 
     const url = 'https://apis.data.go.kr/B551011/KorService1/searchKeyword1';
@@ -16,18 +17,24 @@ export default async function handler(req, res) {
     try {
         const response = await axios.get(url, {
             params: {
-                numOfRows: 5,
+                numOfRows: 1, // 1개의 항목만 필요
                 pageNo: 1,
                 MobileOS: 'ETC',
                 _type: 'json',
                 MobileApp: '트리플러터',
-                keyword: region,
+                keyword: title || region,
                 contentTypeId: 12,
                 serviceKey: serviceKey,
             },
         });
 
-        res.status(200).json(response.data.response.body.items.item);
+        const items = response.data.response.body.items.item;
+
+        if (!items || items.length === 0) {
+            return res.status(404).json({ message: 'No data found' }); // 데이터가 없을 때 처리
+        }
+
+        res.status(200).json(items[0]); // 단일 항목 반환
     } catch (error) {
         res.status(500).json({ message: 'Failed to fetch data', error: error.message });
     }
