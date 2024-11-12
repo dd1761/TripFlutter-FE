787ec08049fa957fff02cd411ec5bd76b0da1ea4
chaerin.jeon <chaerin.jeon@hlcompany.com>
2024-11-13 01:41:32 +0900

    Feat: Add CreateTripPage page

--- app/trip/create/page.tsx
+++ app/trip/create/page.tsx
@@ -1,9 +1,15 @@
 "use client";
-import styled from "styled-components";
 import DefaultLayout from "@/app/components/DefaultLayout";
+import CommonHeader from "@/app/components/commons/CommonHeader";
+import Chat from "@/app/components/commons/Chat";
 
 const CreateTripPage: React.FC = () => {
-  return <></>;
+  return (
+    <DefaultLayout top={0} right={0} bottom={0} left={0} nav={true}>
+      <CommonHeader backLink={"/"} headerTitle={"채팅 여정 생성 페이지"} />
+      <Chat />
+    </DefaultLayout>
+  );
 };
 
 export default CreateTripPage;
